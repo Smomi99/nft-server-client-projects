bc66e898737f869875419a50bfd8e340b0279b4b
Smomi99 <sadi15-9665@diu.edu.bd>
2022-03-07 14:01:14 +0600

    final

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, NavDropdown, NavLink } from "react-bootstrap";
 import "./Header.css";
 import footerLogo from "../../assets/logo1.png";
 import menu from "../../assets/menu.png";
@@ -14,7 +14,7 @@ const Header = () => {
         <Container
           fluid
         >
-          <Navbar.Brand>
+          <Navbar.Brand >
 
             <div style={{ width: "52px" }}>
               <Link to="/"> <img style={{ width: "100%" }} src={footerLogo} alt="" /></Link>
@@ -23,51 +23,42 @@ const Header = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className="p-4">
-                <Link className="text-decoration-none text-white" to="/">
-                  <img src={menu} alt="" />
-                </Link>
-              </Nav.Link>
-              <Nav.Link className="p-4">
-                <Link
-                  className="text-decoration-none text-white"
-                  to="/marketplace"
-                >
-                  <div className="d-flex gap-2">
-                    <img
-                      className="img-fluid"
-                      style={{ width: "20px", height: "23px" }}
-                      src={diamond}
-                      alt=""
-                    />
-                    <h6 className="text-white">MARKETPLACE</h6>
-                  </div>
-                </Link>
-              </Nav.Link>
-              <Nav.Link className="p-4 border-1">
-                <Link className="text-decoration-none text-white" to="/buynow">
-                  <div className="d-flex gap-2">
-                    <img className="img-fluid" src={staking} alt="" />
-                    <h6 className="text-white">Staking</h6>
-                  </div>
-                </Link>
-              </Nav.Link>
-              <Nav.Link className="p-4">
-                <Link
-                  className="text-decoration-none text-white"
-                  to="/marketplace"
-                >
-                  <div className="d-flex gap-2">
-                    <img
-                      className="img-fluid"
-                      style={{ width: "20px", height: "20px" }}
-                      src={news}
-                      alt=""
-                    />
-                    <h6 className="text-white">News</h6>
-                  </div>
-                </Link>
-              </Nav.Link>
+
+              <Link to="/" className="activebtn p-4">
+
+                <img src={menu} alt="" />
+
+              </Link>
+              <Link to="/marketplace" className="activebtn p-4 text-decoration-none">
+                <div className="d-flex gap-2">
+                  <img
+                    className="img-fluid"
+                    style={{ width: "20px", height: "23px" }}
+                    src={diamond}
+                    alt=""
+                  />
+                  <h6 className="text-white">MARKETPLACE</h6>
+                </div>
+              </Link>
+
+              <Link to="/buynow" className="activebtn p-4 text-decoration-none">
+                <div className="d-flex gap-2">
+                  <img className="img-fluid" src={staking} alt="" />
+                  <h6 className="text-white">Staking</h6>
+                </div>
+              </Link>
+              <Link to="/marketplace" className="activebtn p-4 text-decoration-none">
+
+                <div className="d-flex gap-2">
+                  <img
+                    className="img-fluid"
+                    style={{ width: "20px", height: "20px" }}
+                    src={news}
+                    alt=""
+                  />
+                  <h6 className="text-white">News</h6>
+                </div>
+              </Link>
             </Nav>
             <Nav>
               <Button className="btn">CONNECT WALLET</Button>
