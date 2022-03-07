832a9f6511faa13055ffc1c3612e3c0937d9bfe4
Smomi99 <sadi15-9665@diu.edu.bd>
2022-03-07 13:34:30 +0600

    final comm

--- src/pages/FirstMenuBody/FirstMenuBody.js
+++ src/pages/FirstMenuBody/FirstMenuBody.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import m1 from "../../assets/m1.png";
-import arrowdown from "../../assets/arrow-down.png";
 import di1 from "../../assets/di1.png";
 import dr1 from "../../assets/dr1.png";
 import arm from "../../assets/arm.png";
@@ -9,7 +8,6 @@ import git from "../../assets/git.png";
 import wiz from "../../assets/wiz.png";
 import updownarr from "../../assets/updownarr.png";
 import "./FirstMenu.css";
-import FirstMenuAccordation from "../FirstMenuAccordation/FirstMenuAccordation";
 import Pagination from "../Pagination/Pagination";
 
 const FirstMenuBody = () => {
@@ -209,110 +207,20 @@ const FirstMenuBody = () => {
             <div class="accordion-item">
               <h2 class="accordion-header" id="panelsStayOpen-headingThree">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                  <h6>WEAPONS</h6>
+                  <h6>EQUIPMENTS</h6>
                 </button>
               </h2>
               <div style={{ backgroundColor: '#020d32 ' }} id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                 <div className="d-flex gap-4 ms-2">
-                  <div className="d-flex druios mt-4 justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex mt-4 druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex mb-4 druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
+                  <div className="d-flex gap-4 ms-4 mt-4 mb-4">
+                    <div className="d-flex justify-content-between equip align-items-center">
+                      <h5 className="text-white">4568</h5>
+                      <img src={updownarr} alt="" />
+                    </div>
+                    <div className="d-flex justify-content-between equip align-items-center">
+                      <h5 className="text-white">4568</h5>
+                      <img src={updownarr} alt="" />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -320,111 +228,37 @@ const FirstMenuBody = () => {
             <div class="accordion-item">
               <h2 class="accordion-header" id="panelsStayOpen-headingFour">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
-                  <h6>WEAPONS</h6>
+                  <h6>PETS</h6>
                 </button>
               </h2>
               <div style={{ backgroundColor: '#020d32 ' }} id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
-                <div className="d-flex gap-4 ms-2">
-                  <div className="d-flex druios mt-4 justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex mt-4 druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
+
+                <div className="d-flex gap-2 ">
+                  <h5 className="text-white ms-2 pets text-center">2006</h5>
+                  <h5 className="text-white ms-2 pets text-center">2007</h5>
+                  <h5 className="text-white ms-2 pets text-center">2008</h5>
+                  <h5 className="text-white ms-2 pets text-center">2009</h5>
                 </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
-                  </div>
+                <div className="d-flex gap-2 ">
+                  <h5 className="text-white ms-2 pets text-center">2010</h5>
+                  <h5 className="text-white ms-2 pets text-center">2011</h5>
+                  <h5 className="text-white ms-2 pets text-center">2012</h5>
+                  <h5 className="text-white ms-2 pets text-center">2013</h5>
                 </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
+                <div className="d-flex gap-2 ">
+                  <h5 className="text-white ms-2 pets text-center">2014</h5>
+                  <h5 className="text-white ms-2 pets text-center">2015</h5>
+                  <h5 className="text-white ms-2 pets text-center">2016</h5>
+                  <h5 className="text-white ms-2 pets text-center">2017</h5>
                 </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
-                  </div>
+                <div className="d-flex gap-2 ">
+                  <h5 className="text-white ms-2 pets text-center">2018</h5>
+                  <h5 className="text-white ms-2 pets text-center">2019</h5>
+                  <h5 className="text-white ms-2 pets text-center">2020</h5>
+                  <h5 className="text-white ms-2 pets text-center">2021</h5>
                 </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={di1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DISTANCE FIGHTER
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={dr1} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      DRUIOS
-                    </p>
-                  </div>
-                </div>
-                <div className="d-flex gap-4 mt-2 ms-2">
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={arm} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      ARMORED
-                    </p>
-                  </div>
-                  <div className="d-flex druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={wiz} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      WIZAROS
-                    </p>
-                  </div>
-                  <div className="d-flex mb-4 druios justify-content-evenly">
-                    <img style={{ height: "20px" }} src={git} alt="" />
-                    <p style={{ fontSize: "12px" }} className="text-white">
-                      HARMONY
-                    </p>
-                  </div>
+                <div className="d-flex gap-2 ">
+                  <h5 className="text-white ms-2 pets text-center">2022</h5>
                 </div>
               </div>
             </div>
